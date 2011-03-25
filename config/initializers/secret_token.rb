d4b0e9018bcc96ec4dfe0c28d8138fdfd7303aa6
Lucas <lukspn.27@gmail.com>
2011-03-25 18:35:08 -0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Letsgo::Application.config.secret_token = 'a823b6fe999789902f151514372f2fc55b0b4f52e583d25f5cc64794ebd5445b0f3b82b88ee02ec3dc59ba39bf3fb511239ba98f44a0e5259b725152e3fd8d0f'
